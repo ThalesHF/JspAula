4f383d92c4615de7dfd4a8f085ff86baee5b30d7
Aluno <Aluno@045lab04029.up.edu>
2017-10-24 22:09:46 -0200

    Commit

---
+++ JSPServletCRUDProduto/src/dal/ProdutoDAO.java
@@ -0,0 +1,33 @@
+package dal;
+
+import java.util.ArrayList;
+
+import model.Produto;
+
+public class ProdutoDAO {
+
+	private static ArrayList<Produto> produtos = new ArrayList<Produto>();
+
+	public static boolean cadastrarProduto(Produto produto){
+		if(produtos.size()>0){
+		int id = produtos.get(produtos.size()- 1).getId()+ 1;
+		produto.setId(id);
+		}else{
+			produto.setId(1);
+		}
+		if(buscarProdutoPorNome (produto) == null){
+			produtos.add(produto);
+			return true;
+		}
+		return false;
+	}
+
+	public static Produto buscarProdutoPorNome (Produto produto){
+		for (Produto produtoCadastrado : produtos){
+			if (produtoCadastrado.getNome().equals(produto.getNome())){
+				return produtoCadastrado;
+			}
+		}
+		return null;
+	}
+}
